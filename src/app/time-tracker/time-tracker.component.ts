d3990cce4cca09e348fba1ddaafaba346572e7fd
sarget <sarget.alexis@orange.fr>
2020-05-25 21:50:13 +0200

    Code commente.

--- src/app/time-tracker/time-tracker.component.ts
+++ src/app/time-tracker/time-tracker.component.ts
@@ -16,43 +16,53 @@ import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/form
 })
 export class TimeTrackerComponent implements OnInit, OnDestroy {
 
-  edited = false;
-  editForm: FormGroup;
-  editTache: Tache = null;
+  edited = false; // Pour savoir quelle tache est seletionnee
+  editForm: FormGroup; // Formulaire pour modifier une tache
+  editTache: Tache = null; // La tache que l'on veut modifier
 
-  showChildren: boolean[] = [];
+  showChildren: boolean[] = []; // Pour savoir sur quelle categorie on clique pour afficher ses taches
 
-  categories: Categorie[];
-  taches: Tache[];
-  categoriesSubscription: Subscription;
-  tachesSubscription: Subscription;
+  categories: Categorie[]; // Les categories presentent en base
+  taches: Tache[]; // Les taches presentent en base
+  categoriesSubscription: Subscription; // L'observateur des categories pour actualiser categories
+  tachesSubscription: Subscription; // L'observateur des taches pour actualiser taches
 
   // idTache: number;
-  compteurTache: number[];
-  subsTempsTache: Subscription[] = [];
+  compteurTache: number[]; // Pour faire defiler le temps qui passe pour chaque taches
+  subsTempsTache: Subscription[] = []; // L'observateur pour actualiser le compteur d'une tache
   // dateActive: Date[];
-  saveForm: FormGroup;
-  tree: Map<object, any>;
+  saveForm: FormGroup; // Formulaire pour enregister une tache ou une categorie
 
+  /**
+   * Crée une instance de time-tracker
+   * @param TachesService    tacheService     Le service peremetant d'interagir avec les taches
+   * @param FormBuilder      formBuilder      Permet de creer les formulaires
+   * @param CategorieService categorieService Le service peremetant d'interagir avec les categories
+   */
   constructor(private tachesService: TachesService, private formBuilder: FormBuilder, private categorieService: CategorieService) {
   }
 
   ngOnInit(): void {
+    // S'abonne au sujet categorie
     this.categoriesSubscription = this.categorieService.categorieSubject.subscribe(
       (categories: Categorie[]) => {
         this.categories = categories;
       }
     );
     this.categorieService.emitCategorie();
+    // S'abonne au sujet tache
     this.tachesSubscription = this.tachesService.tachesSubject.subscribe(
       (taches: Tache[]) => {
         this.taches = taches;
       }
     );
     this.tachesService.emitTaches();
+
+    // Initialise les formualaires
     this.initFormSave();
     this.initFormEdited();
 
+    // Initialise toutes les categories fermee (on ne voit pas les taches qui leurs sont associées)
     this.categories.forEach(() => {
       this.showChildren.push(false);
     });
@@ -67,6 +77,7 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
       // }));
     });*/
 
+    // Initialise l'observale des compteurs des taches
     this.compteurTache = [];
     this.taches.forEach(() => {
       this.subsTempsTache.push(new Subscription());
@@ -78,6 +89,9 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     });
   }
 
+  /**
+   * Initialise le formulaire pour creer une tache/categorie
+   */
   initFormSave() {
     this.saveForm = this.formBuilder.group({
       type: ['', Validators.required],
@@ -86,6 +100,9 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     });
   }
 
+  /**
+   * Initialise le formulaire pour editer les taches
+   */
   initFormEdited() {
     this.editForm = this.formBuilder.group({
       nveauTitre: [''],
@@ -111,6 +128,9 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     });*/
   }
 
+  /**
+   * Est appele lors de la validation du formulaire de creation de tache/categorie (saveForm)
+   */
   onSave() {
     if (this.saveForm.get('type').value === 'tache') {
       this.onSaveTache();
@@ -119,6 +139,9 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Permet a l'utilisateur de creer une tache
+   */
   onSaveTache() {
     const titre = this.saveForm.get('title').value;
     const temps = 0;
@@ -130,18 +153,25 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     this.tachesService.createNewTache(newTache);
   }
 
+  /**
+   * Permet a l'utilisateur de creer une quick task
+   */
   onQuickStart() {
     const titre = 'Quick Start '/* + ++this.nbQuickStartTaches*/;
     const temps = 0;
     const estDemaree = false;
     // const Date1 = new Date();
     // const Date2 = new Date();
-    const parent = -1; // id single tasks
+    const parent = -1; // Pas de categorie (uniquement present dans All tasks)
     const newTache = new Tache(titre, temps, estDemaree/*, Date1, Date2*/ , parent);
     this.tachesService.createNewQuickStartTache(newTache);
     this.demarerStopperTache(this.taches[this.taches.length - 1]);
   }
 
+  /**
+   * Permet a l'utilisateur de creer une categorie
+   * TODO: Gestion des erreurs
+   */
   onSaveCategorie() {
     const titre = this.saveForm.get('title').value;
     const categorieIndexToFind = this.categories.findIndex(
@@ -158,11 +188,17 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
       const newCategorie = new Categorie(titre, temps, parent);
       this.categorieService.createNewCategorie(newCategorie);
     } else {
+      // Nom de la categorie existe deja
       console.log('error');
     }
   }
 
+  /**
+   * Permet d'editier le nom et la categorie d'une tache
+   * @param Tache tache La tache que l'on veut modifier
+   */
   onEdit(tache: Tache) {
+    // Si la tache est une quick task
     if (tache.parent === -1) {
       this.categories[this.getCategorieIdByName(this.editForm.get('nvelleCate').value)].temps += tache.temps;
       this.categorieService.updateTemps(this.categories[this.getCategorieIdByName(this.editForm.get('nvelleCate').value)]);
@@ -195,8 +231,14 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
   //   });
   // }
 
+  /**
+   * Permet de recuperer toutes la tache d'une categorie
+   * @param number categorieId L'id de la categorie
+   * @return Tache[] La liste des taches associees a categorieId
+   */
   getCategorieChildren(categorieId: number) {
     const res = [];
+    // Si la categorie est All tasks alors on recupere toutes les taches
     if (categorieId === 0) {
       return this.taches;
     }
@@ -208,6 +250,11 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     return res;
   }
 
+  /**
+   * Permet de recuperer l'id d'une categorie avec son nom
+   * @param string categorieNom Le nom de la categorie
+   * @return number L'id de la categorie ayant le nom categorieNom
+   */
   getCategorieIdByName(categorieNom: string) {
     const categorieIndexToFind = this.categories.findIndex(
       (categorieE1) => {
@@ -219,6 +266,10 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     return categorieIndexToFind;
   }
 
+  /**
+   * Permet de recuperer toutes les taches demarees
+   * @return Tache[] Les taches demarees
+   */
   getTachesDemaree() {
     const res = [];
     this.taches.forEach((tache) => {
@@ -229,6 +280,10 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     return res;
   }
 
+  /**
+   * Permet d'afficher les taches associees a une categorie
+   * @param number indice L'id de la categorie
+   */
   showTacheOfCategorie(indice: number) {
     let i = 0;
     this.showChildren.forEach(() => {
@@ -266,6 +321,10 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
   //   this.idTache++;
   // }
 
+  /**
+   * Permet de d'inverser l'etat d'une tache (si elle est demaree elle se met en pause et inversement)
+   * @param Tache tache La tache a laquelle on veut modifier son etat
+   */
   demarerStopperTache(tache: Tache) {
     // console.log(this.taches.length);
     // console.log(this.editForm.length);
@@ -288,6 +347,7 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
       //   this.tachesService.saveQuickStart(tache, );
       // } else {
       // }
+      // Si la tache est une quick task, alors quand on la met en pause alors il faut lui donner un nom et une categorie
       if (tache.parent === -1) {
         this.modifierTache(tache);
       } else {
@@ -301,11 +361,19 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     }
   }
 
+  /**
+   * Permet de demarrer le timer d'une tache
+   * @param Tache tache la tache que l'on veut lancer
+   */
   demarerTache(tache: Tache) {
     const indice = this.taches.indexOf(tache);
     this.subsTempsTache[indice] = interval(1000).subscribe((valeur: number) => (this.compteurTache[indice] = valeur));
   }
 
+  /**
+   * Permet de stopper le timer d'une tache et actualise le timer de sa categorie
+   * @param Tache tache
+   */
   stopperTache(tache: Tache) {
     const indice = this.taches.indexOf(tache);
     tache.temps += this.compteurTache[indice];
@@ -341,6 +409,10 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
   //   }
   // }
 
+  /**
+   * Permet d'actualiser toutes les secondes le timer le timer d'une tache (Affichage)
+   * @param Tache tache La tache a laquelle on veut actualier le timer
+   */
   tempsDynamiqueTache(tache: Tache/*, i: number*/) {
     const i = this.taches.indexOf(tache);
     const date = new Date(0);
@@ -349,6 +421,10 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     // return this.compteur[i] ? tache.temps + this.compteur[i] : tache.temps;
   }
 
+  /**
+   * Actualise le timer d'une categorie
+   * @param Categorie categorie La categorie a laquelle on veut modifier le timer
+   */
   tempsCategorie(categorie: Categorie) {
     const date = new Date(0);
     date.setSeconds( categorie.temps); // specify value for SECONDS here
@@ -356,10 +432,16 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     // return this.compteur[i] ? tache.temps + this.compteur[i] : tache.temps;
   }
 
+  /**
+   * Permet de supprimer une tache
+   * @param Tache tache La tache que l'on veut supprimer
+   */
   supprimerTache(tache: Tache) {
+    // Si la tache est demarre on la stop avant de la supprimer
     if (tache.estDemaree) {
       this.stopperTache(tache);
     }
+    // Si la tache n'est pas une quick task on modifie le timer de la categorie associee
     if (tache.parent !== -1) {
       this.categories[tache.parent].temps -= tache.temps;
       if (this.categories[tache.parent].temps < 0) {
@@ -367,6 +449,7 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
       }
       this.categorieService.updateTemps(this.categories[tache.parent]);
     }
+    // Modifie le timer la categorie de All tasks
     this.categories[this.getCategorieIdByName('All tasks')].temps -= tache.temps;
     if (this.categories[this.getCategorieIdByName('All tasks')].temps < 0) {
       this.categories[this.getCategorieIdByName('All tasks')].temps = 0;
@@ -375,6 +458,10 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     this.tachesService.removeTache(tache);
   }
 
+  /**
+   * Permet de supprimer une categorie est toutes les taches qui lui sont associes
+   * @param Categorie acategorie La categorie que l'on veut supprimer
+   */
   supprimerCategorie(categorie: Categorie) {
     const id = this.getCategorieIdByName(categorie.titre);
     const children = this.getCategorieChildren(id);
@@ -384,6 +471,10 @@ export class TimeTrackerComponent implements OnInit, OnDestroy {
     this.categorieService.removeCategorie(categorie);
   }
 
+  /**
+   * Permet de modifier une tache en pré chargeant ses données dans le formulaire
+   * @param Tache tache la tache que l'on va modifier
+   */
   modifierTache(tache: Tache) {
     // this.initFormEdited();
     this.editTache = tache;
